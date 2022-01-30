70d3d5c86fa24a307865b51278c353fe02599fdb
CraigThomasson <c.a.thomasson@hotmail.co.uk>
2022-01-30 22:18:26 +0000

    fix issues with deploy

--- WELLBEING/settings.py
+++ WELLBEING/settings.py
@@ -25,7 +25,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates', 'allauth')
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
+
+
 
 ALLOWED_HOSTS = ["centered-ci.herokuapp.com", "localhost"]
 
