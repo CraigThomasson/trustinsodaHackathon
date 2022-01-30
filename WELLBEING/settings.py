5b4ac035a61b4e75a20059e9e360c4201efeeb8e
CraigThomasson <c.a.thomasson@hotmail.co.uk>
2022-01-30 21:53:17 +0000

    fix dep

--- WELLBEING/settings.py
+++ WELLBEING/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["WELLBEING.herokuapp.com", "localhost"]
+ALLOWED_HOSTS = ["centered-ci.herokuapp.com", "localhost"]
 
 
 # Application definition
