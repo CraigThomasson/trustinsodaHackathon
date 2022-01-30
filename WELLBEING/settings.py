df734324c9925e8312005eb92c07b5cdd75fb111
CraigThomasson <c.a.thomasson@hotmail.co.uk>
2022-01-30 22:24:12 +0000

    more heroku

--- WELLBEING/settings.py
+++ WELLBEING/settings.py
@@ -25,9 +25,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates', 'allauth')
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
-
-
+DEBUG = True
 
 ALLOWED_HOSTS = ["centered-ci.herokuapp.com", "localhost"]
 
