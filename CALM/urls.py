70e24616a7ea7f42dc2172bc981e14b1c068e899
JoyZadan <joyzadan.learning@gmail.com>
2022-01-30 10:33:54 +0000

    added content to 8 steps

--- CALM/urls.py
+++ CALM/urls.py
@@ -1,8 +1,8 @@
-from .views import index
+from .views import index, solutions
 from django.urls import path
 
 urlpatterns = [
 
     path('',  index),
-    path('',  solutions),
+    path('solutions.html',  solutions),
 ]
