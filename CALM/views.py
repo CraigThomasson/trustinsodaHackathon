fe9fb38f04f5b08fabaaf3436478bc7b89e30e8a
declanosullivan <declanseer@gmail.com>
2022-01-30 15:05:18 +0000

    Fix base.html issue

--- CALM/views.py
+++ CALM/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from django.shortcuts import render
 
 def base(request):
-    return render(request, 'base.html')
+    return render(request, 'site_base.html')
 
 def solutions(request):
     return render(request, 'solutions.html')
