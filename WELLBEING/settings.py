1ad5afd316f47b59d5f5feb9ef2d03e90941adee
CraigThomasson <c.a.thomasson@hotmail.co.uk>
2022-01-30 21:48:50 +0000

    fixing herku dep

--- WELLBEING/settings.py
+++ WELLBEING/settings.py
@@ -17,10 +17,7 @@ if os.path.isfile("env.py"):
     import env
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-TEMPLATES_DIR = [
-    os.path.join(BASE_DIR, 'templates'),
-    os.path.join(BASE_DIR, 'templates', 'allauth'),
-    ]
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates', 'allauth')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
